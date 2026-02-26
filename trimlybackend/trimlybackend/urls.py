36cb59dd6cc9c3b6f008d33c74beb443d9b1fcb4
Daniel Favour <feivodanny.06@gmail.com>
2026-02-26 11:41:36 +0100

    major changes

--- trimlybackend/trimlybackend/urls.py
+++ trimlybackend/trimlybackend/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('api/v1/search/', include('api.v1.Search.urls')),
     path('api/v1/toggle-status/', toggle_availability, name='toggle-availability'),
     path('api/v1/reviews/', include('api.v1.Reviews.urls')),
-    path('api/v1/cha/', include('api.v1.Reviews.urls')),
+    path('api/v1/chat/', include('api.v1.Chat.urls')),
     #documentation
     path('swagger/', SpectacularSwaggerView.as_view(), name="schema"),
     path('redoc/', SpectacularRedocView.as_view(), name="redoc"),
